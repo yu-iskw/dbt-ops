84162b6e6b0cdcefc1125732eea78c5d59778557
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2023-12-20 08:37:56 +0900

    Rename `get_unreferenced_sources` to `find_unreferenced_sources`
    Signed-off-by: Yu Ishikawa <yu-iskw@users.noreply.github.com>

--- integration_tests/macros/tests/test_find_unreferenced_sources.sql
+++ integration_tests/macros/tests/test_find_unreferenced_sources.sql
@@ -14,7 +14,7 @@
   limitations under the License.
 #}
 
-{% macro test_get_unrendered_sources() %}
-  {% set unreferenced_sources = dbt_ops.__get_unreferenced_sources() %}
+{% macro test_find_unrendered_sources() %}
+  {% set unreferenced_sources = dbt_ops.__find_unreferenced_sources() %}
   {% do assert_equals(unreferenced_sources | length, 1) %}
 {% endmacro %}
