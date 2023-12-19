55247dced0066ebebdc3e355b7febb920b7c532d
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2023-12-19 23:19:18 +0900

    Add license headers (#3)
    Signed-off-by: Yu Ishikawa <yu-iskw@users.noreply.github.com>

--- integration_tests/macros/test_utils/asserts.sql
+++ integration_tests/macros/test_utils/asserts.sql
@@ -1,3 +1,19 @@
+{#
+  Copyright 2023 yu-iskw
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+#}
+
 {% macro assert_true(value) %}
   {% if value is boolean and value is not true %}
     {% do exceptions.raise_compiler_error("FAILED: value " ~ value ~ " is not true.") %}
